fdf0caa85bbd504c3f2c33d14f376f7511074bd6
Louie Casula <121182711+louiecasula@users.noreply.github.com>
2024-03-11 14:00:02 -0400

    Made operate function to choose between existing arithmetic operations and return result.

--- script.js
+++ script.js
@@ -19,5 +19,26 @@ function modulus(num1, num2) {
     return (num2 !== 0)? num1 % num2: undefined;
 }
 
+// Current operation:
+let num1 = 6;
+let operator = "%";
+let num2 = 2;
+
+function operate(num1, operator, num2) {
+    switch(operator) {
+        case("+"):
+            return add(num1, num2);
+        case("-"):
+            return subtract(num1, num2);
+        case("*"):
+            return multiply(num1, num2);
+        case("/"):
+            return divide(num1, num2);
+        case("%"):
+            return modulus(num1, num2);        
+    };
+};
+
+
 // Display:
-console.log(modulus(9, -2));
+console.log(num1 + " " + operator + " " + num2 + " = " + operate(num1, operator, num2));
